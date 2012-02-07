dd1401db4cd595b56443433d89f45e03e52061c1
armin <git@eulerpfad.com>
2012-02-08 00:09:34 +0100

    cleanup of flutter project directory

--- hardware/flutter/flutter0/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/home/gjehle/Development/shackspace/hgg/hardware/flutter/flutter0/timesrc_test_isim_beh.exe" -prj "/home/gjehle/Development/shackspace/hgg/hardware/flutter/flutter0/timesrc_test_beh.prj" "work.timesrc_test" 
