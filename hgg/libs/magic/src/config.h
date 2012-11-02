ce1709c6ed7723616c1ec1c3a7da6fecb4dbc862
hdznrrd <hadez@nrrd.de>
2012-11-02 20:03:27 +0100

    compiletime configuration files

---
+++ hgg/libs/magic/src/config.h
@@ -0,0 +1,12 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define HGG_BACKPLANE_FRIENDSHIP0 ///< backplane: friendship0 prototype
+
+
+#ifdef HGG_BACKPLANE_FRIENDSHIP0
+#include "config/config_friendship0.h"
+#endif
+
+
+#endif //_CONFIG_H_
